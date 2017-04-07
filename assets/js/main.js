5b967f07ce131246c78ee6dbd1e9c5bd00be5eda
Marilu Llamocca <maryleo.3007@gmail.com>
2017-04-07 17:16:57 -0500

    todas las coders

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,6 @@ function printCoderHtml(parent){
     parent.innerHTML = "";
     coders.forEach(function(elemento){
         parent.appendChild(createCodeHtml(elemento.id,elemento.nombre,elemento.foto));
-        //parent.appendChild(createCodeHtml(elemento.id,elemento.foto));
     });
 }
 /*********Funcion para crear el dom para mostrar la lista de coders***********/
@@ -56,21 +55,11 @@ function createCodeHtml(id,txtNombre,srcFoto){
     var span2 = document.createElement("span");
     span2.setAttribute("class","nombre");
     span2.appendChild(document.createTextNode(txtNombre));
-    /*var br1 = document.createElement("br");
-    //var spanPromocion = document.createElement("span");
-    //spanPromocion.appendChild(document.createTextNode(txtPromocion));
-    //spanPromocion.setAttribute("class","spanPromocion");
-    //var br2 = document.createElement("br");
-    //var spanNombre = document.createElement("span");
-    //spanNombre.appendChild(document.createTextNode(txtNombre));
-    //spanNombre.setAttribute("class","spanNombre");*/
+
     li.appendChild(img);
     li.appendChild(span);
     span.appendChild(span2);
-    /*//div.appendChild(br1);
-    //div.appendChild(spanPromocion);
-    //div.appendChild(br2);
-    //div.appendChild(spanNombre);*/
+
     df.appendChild(li);
     return df;
 }
@@ -78,28 +67,59 @@ window.addEventListener('load', function () {
   var allCoders = document.getElementById("listaCoders");
 
     /******Registro de alumnas */
-      var coderLima1 = new Coder(1,"Anais Araya","assets/img/coders/1.png");
-      var coderLima2 = new Coder(2,"Andrea Miranda","assets/img/coders/2.png");
-      var coderLima3 = new Coder(3,"Berenisse Rios","assets/img/coders/3.png");
-      var coderLima4 = new Coder(4,"Caterina da Silva","assets/img/coders/4.png");
-      var coderLima5 = new Coder(5,"Daniela Sanchez","assets/img/coders/5.png");
-      var coderLima6 = new Coder(6,"Francisca Ojeda","assets/img/coders/6.png");
-      var coderLima7 = new Coder(7,"Katerina Sandoval","assets/img/coders/7.png");
-      var coderLima8 = new Coder(8,"Andrea Cabrera","assets/img/coders/8.png");
-      var coderLima9 = new Coder(9,"Ariadna Izaguirre","assets/img/coders/9.png");
-      var coderLima10 = new Coder(10,"Carito Juarez","assets/img/coders/10.png");
-      var coderLima11 = new Coder(11,"Cristy Castro","assets/img/coders/11.png");
-      var coderLima12 = new Coder(12,"Karol Orrillo","assets/img/coders/12.png");
-      var coderLima13 = new Coder(13,"Paola Ortiz","assets/img/coders/13.png");
-      var coderLima14 = new Coder(14,"Teresa Lara","assets/img/coders/14.png");
-      var coderLima15 = new Coder(15,"Arantza Burga","assets/img/coders/15.png");
-      var coderLima16 = new Coder(16,"Daguiana Revoredo","assets/img/coders/16.png");
-      var coderLima17 = new Coder(17,"Elizabeth Condori","assets/img/coders/17.png");
-      var coderLima18 = new Coder(18,"Grecia Rayme","assets/img/coders/18.png");
-      var coderLima19 = new Coder(19,"Janine Vega","assets/img/coders/19.png");
-      var coderLima20 = new Coder(20,"Michelle More","assets/img/coders/20.png");
-      var coderLima21 = new Coder(21,"Misshel Velazquez","assets/img/coders/21.png");
-      var coderLima22 = new Coder(22,"Rosario Felix","assets/img/coders/22.png");
+      var coderLima1 = new Coder(1,"Fiorela Quispe","assets/img/coders/1.png");
+      var coderLima2 = new Coder(2,"Maria Lourdes","assets/img/coders/2.png");
+      var coderLima3 = new Coder(3,"Leydi Maldonado","assets/img/coders/3.png");
+      var coderLima4 = new Coder(4,"Yesenia","assets/img/coders/4.png");
+      var coderLima5 = new Coder(5,"Miriam Mendoza","assets/img/coders/5.png");
+      var coderLima6 = new Coder(6,"Elizabeth Condori","assets/img/coders/6.png");
+      var coderLima7 = new Coder(7,"Arantza Burga","assets/img/coders/7.png");
+      var coderLima8 = new Coder(8,"Grecia Rayme","assets/img/coders/8.png");
+      var coderLima9 = new Coder(9,"Yanine Vega","assets/img/coders/9.png");
+      var coderLima10 = new Coder(10,"Rosario Felix","assets/img/coders/10.png");
+      var coderLima11 = new Coder(11,"Daguiana Revolledo","assets/img/coders/11.png");
+      var coderLima12 = new Coder(12,"Jenny Mishel  Velazquez","assets/img/coders/12.png");
+      var coderLima13 = new Coder(13,"Nadia Cuadros","assets/img/coders/13.png");
+      var coderLima14 = new Coder(14,"Misshel More","assets/img/coders/14.png");
+      var coderLima15 = new Coder(15,"Marilu Llamocca","assets/img/coders/15.png");
+      var coderLima16 = new Coder(16,"Mariel del Rosario","assets/img/coders/16.png");
+      var coderLima17 = new Coder(17,"Fiorela Cisneros","assets/img/coders/17.png");
+      var coderLima18 = new Coder(18,"Geraldine Chauca","assets/img/coders/18.png");
+      var coderLima19 = new Coder(19,"Yelitza Choque","assets/img/coders/19.png");
+      var coderLima20 = new Coder(20,"Sandra","assets/img/coders/20.png");
+      var coderLima21 = new Coder(21,"Enma","assets/img/coders/21.png");
+      var coderLima22 = new Coder(22,"Danna","assets/img/coders/22.png");
+      var coderLima23 = new Coder(23,"Nadia Cuadros","assets/img/coders/13.png");
+      var coderLima24 = new Coder(24,"Misshel More","assets/img/coders/14.png");
+      var coderLima25 = new Coder(25,"Marilu Llamocca","assets/img/coders/15.png");
+      var coderLima26 = new Coder(26,"Mariel del Rosario","assets/img/coders/16.png");
+      var coderLima27 = new Coder(27,"Fiorela Cisneros","assets/img/coders/17.png");
+      var coderLima28 = new Coder(28,"Geraldine Chauca","assets/img/coders/18.png");
+      var coderLima29 = new Coder(29,"Yelitza Choque","assets/img/coders/19.png");
+      var coderLima30 = new Coder(30,"Sandra","assets/img/coders/20.png");
+      var coderLima31 = new Coder(31,"Enma","assets/img/coders/21.png");
+      var coderLima32 = new Coder(32,"Danna","assets/img/coders/22.png");
+      var coderLima33 = new Coder(33,"Nadia Cuadros","assets/img/coders/13.png");
+      var coderLima34 = new Coder(34,"Misshel More","assets/img/coders/14.png");
+      var coderLima35 = new Coder(35,"Marilu Llamocca","assets/img/coders/15.png");
+      var coderLima36 = new Coder(36,"Mariel del Rosario","assets/img/coders/16.png");
+      var coderLima37 = new Coder(37,"Fiorela Cisneros","assets/img/coders/17.png");
+      var coderLima38 = new Coder(38,"Geraldine Chauca","assets/img/coders/18.png");
+      var coderLima39 = new Coder(39,"Yelitza Choque","assets/img/coders/19.png");
+      var coderLima40 = new Coder(40,"Sandra","assets/img/coders/20.png");
+      var coderLima41 = new Coder(41,"Enma","assets/img/coders/21.png");
+      var coderLima42 = new Coder(42,"Danna","assets/img/coders/22.png");
+      var coderLima43 = new Coder(43,"Nadia Cuadros","assets/img/coders/13.png");
+      var coderLima44 = new Coder(44,"Misshel More","assets/img/coders/14.png");
+      var coderLima45 = new Coder(45,"Marilu Llamocca","assets/img/coders/15.png");
+      var coderLima46 = new Coder(46,"Mariel del Rosario","assets/img/coders/16.png");
+      var coderLima47 = new Coder(47,"Fiorela Cisneros","assets/img/coders/17.png");
+      var coderLima48 = new Coder(48,"Geraldine Chauca","assets/img/coders/18.png");
+      var coderLima49 = new Coder(49,"Yelitza Choque","assets/img/coders/19.png");
+      var coderLima50 = new Coder(50,"Sandra","assets/img/coders/20.png");
+      var coderLima51 = new Coder(51,"Enma","assets/img/coders/21.png");
+      var coderLima52 = new Coder(52,"Danna","assets/img/coders/22.png");
+      var coderLima53 = new Coder(53,"Danna","assets/img/coders/22.png");
       coders.push(coderLima1);
       coders.push(coderLima2);
       coders.push(coderLima3);
@@ -122,6 +142,37 @@ window.addEventListener('load', function () {
       coders.push(coderLima20);
       coders.push(coderLima21);
       coders.push(coderLima22);
+      coders.push(coderLima23);
+      coders.push(coderLima24);
+      coders.push(coderLima25);
+      coders.push(coderLima26);
+      coders.push(coderLima27);
+      coders.push(coderLima28);
+      coders.push(coderLima29);
+      coders.push(coderLima30);
+      coders.push(coderLima31);
+      coders.push(coderLima32);
+      coders.push(coderLima33);
+      coders.push(coderLima34);
+      coders.push(coderLima35);
+      coders.push(coderLima36);
+      coders.push(coderLima37);
+      coders.push(coderLima38);
+      coders.push(coderLima39);
+      coders.push(coderLima40);
+      coders.push(coderLima41);
+      coders.push(coderLima42);
+      coders.push(coderLima43);
+      coders.push(coderLima44);
+      coders.push(coderLima45);
+      coders.push(coderLima46);
+      coders.push(coderLima47);
+      coders.push(coderLima48);
+      coders.push(coderLima49);
+      coders.push(coderLima50);
+      coders.push(coderLima51);
+      coders.push(coderLima52);
+      coders.push(coderLima53);
     /*****Fin del registro de alumnas*****/
 
     printCoderHtml(allCoders);
